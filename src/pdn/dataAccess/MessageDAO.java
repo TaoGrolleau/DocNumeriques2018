b25a8b6bd95b323e51afeee7ac7a253097605824
sarahlaurax <sarah.laurax@gmail.com>
2018-12-03 08:33:18 +0100

    bdd message

--- src/pdn/dataAccess/MessageDAO.java
+++ src/pdn/dataAccess/MessageDAO.java
@@ -12,24 +12,24 @@ import pdn.DatabaseController;
 import static pdn.DatabaseController.connection;
 import pdn.Models.Message;
 
-public class MessageDAO extends DatabaseController{
+public class MessageDAO extends DatabaseController {
 
     public static List<Message> getAllMessageForPersonne(Integer numeroAuthoristion) {
         List<Message> messages = new ArrayList<>();
         try {
             Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT m.id_message, m.id_message_parent, m.statut, m.typeMessage " +
-                    "FROM message m, messages_a_personne mp " +
-                    "WHERE mp.id_personne_ref = " + numeroAuthoristion + " " +
-                    "AND mp.id_message_ref = m.id_message;");
+            ResultSet rs = statement.executeQuery("SELECT m.id_message, m.id_message_parent, m.statut, m.typeMessage "
+                    + "FROM message m, messages_a_personne mp "
+                    + "WHERE mp.id_personne_ref = " + numeroAuthoristion + " "
+                    + "AND mp.id_message_ref = m.id_message;");
             while (rs.next()) {
-                
+
                 Message message = new Message();
                 message.setIdMessage(rs.getInt("id_message"));
                 message.setIdMessageParent(rs.getInt("id_message_parent"));
                 message.setStatut(rs.getString("statut"));
                 message.setTypeMessage(rs.getString("typeMessage"));
-                
+
                 messages.add(message);
             }
         } catch (SQLException ex) {
@@ -37,12 +37,12 @@ public class MessageDAO extends DatabaseController{
         }
         return messages;
     }
-    
-     public static void update(Message m) {
+
+    public static void update(Message m) {
         String sql = "UPDATE message SET statut = ? WHERE id_message = ?; ";
- 
+
         try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
- 
+
             pstmt.setString(1, m.getStatut());
             pstmt.setLong(2, m.getIdMessage());
             pstmt.executeUpdate();
@@ -50,24 +50,32 @@ public class MessageDAO extends DatabaseController{
             Logger.getLogger(PersonneDAO.class.getName()).log(Level.SEVERE, "update error", ex);
         }
     }
-     
-     public static void insererMessage(Long idMessage, Long idMessageParent, String type){
+
+    public static int insererMessage(Long idMessageParent, String type) {
+        int idMessage=0;
         try {
-            Statement statement = connection.createStatement();
-            if(idMessageParent != null)
-                statement.execute("insert into message (id_message, id_message_parent, statut, typeMessage) values ("+ idMessage + ", "+ idMessageParent +", 'non_lu', \'" + type + "\'); ");
-        else
-                statement.execute("insert into message (id_message, statut, typeMessage) values ("+ idMessage + ", 'non_lu', \'"+ type +"\'); ");
+            PreparedStatement statement;
+            if (idMessageParent != null) {
+                statement = connection.prepareStatement("insert into message (id_message_parent, statut, typeMessage) values (" + idMessageParent + ", 'non_lu', \'" + type + "\'); ", Statement.RETURN_GENERATED_KEYS);
+            } else {
+                statement = connection.prepareStatement("insert into message (statut, typeMessage) values ('non_lu', \'" + type + "\'); ");
+            }
+            statement.execute();
+            ResultSet rs = statement.getGeneratedKeys();
+            if(rs.next()){
+                idMessage = rs.getInt("id_message");
+            }
         } catch (SQLException ex) {
             Logger.getLogger(MessageDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-     }
-        
-    public static void associerMessagePersonne(Long idMessage, int idPersonne){
+        return idMessage;
+    }
+
+    public static void associerMessagePersonne(Long idMessage, int idPersonne) {
         Statement statement;
         try {
             statement = connection.createStatement();
-            statement.execute("insert into messages_a_personne values (" + idPersonne + ", " + idMessage + ");" );
+            statement.execute("insert into messages_a_personne values (" + idPersonne + ", " + idMessage + ");");
         } catch (SQLException ex) {
             Logger.getLogger(ObjetDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
